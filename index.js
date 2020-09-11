b20fa027080705c6ffe98c7bc70040bdeb5aa2e1
Dylan Best <best.dylan15@gmail.com>
2020-09-10 22:53:24 -0400

    added new department and role functions

--- index.js
+++ index.js
@@ -19,13 +19,14 @@ connection.connect(function (err) {
     start();
 });
 
+// starts application
 function start() {
     inquirer.prompt([
         {
             type: "list",
             name: "start",
-            message: "Pick an action-",
-            chopices: [
+            message: "Choose an action-",
+            choices: [
                 "add a new department",
                 "add a new role",
                 "add a new employee",
@@ -36,7 +37,7 @@ function start() {
             ]
         }
     ]).then(function (answer) {
-        if(answer.start === "add a new department") {
+        if (answer.start === "add a new department") {
             addDepartment();
         } else if (answer.start === "add a new role") {
             addRole();
@@ -51,5 +52,72 @@ function start() {
         } else {
             updateEmployeeRoles();
         }
+    })
+}
+
+// adds department and restarts
+function addDepartment() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Enter the department name."
+        }
+    ]).then(function (answer) {
+        console.log("Adding new department...");
+        connection.query("INSERT INTO department SET ?",
+            { name: answer.name },
+            function (err, res) {
+                if (err) throw err;
+                console.log("New department added successfully!");
+                start();
+            }
+        )
+    })
+}
+
+function addRole() {
+    connection.query("SELECT * FROM department", function (err, res) {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "title",
+                message: "Enter the role title."
+            },
+            {
+                type: "input",
+                name: "salary",
+                message: "Enter the role salary.",
+                validate: function (value) {
+                    if (isNaN(value)) {
+                        return false;
+                    }
+                    return true;
+                }
+            },
+            {
+                type: "list",
+                name: "department",
+                message: "Choose the role department",
+                choices: function () {
+                    var departmentArray = [];
+                    if (err) throw err;
+                    res.forEach(element => {
+                        departmentArray.push(element.name);
+                    })
+                    return departmentArray;
+                }
+            }
+        ]).then(function (answer) {
+            console.log("Adding new role...");
+            var query = "INSERT INTO role SET title = ?, salary = ?,";
+            query += "department_id = (SELECT id FROM department WHERE name = ?)";
+            connection.query(query, [answer.title, answer.salary, answer.department ], function (err, res) {
+                    if (err) throw err;
+                    console.log("New role added successfully!");
+                    start();
+                }
+            )
+        })
     })
 }
