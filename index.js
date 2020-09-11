5880642333be81b73d89bdfc2ff02a0af0cbd2f3
Dylan Best <best.dylan15@gmail.com>
2020-09-10 21:32:32 -0400

    created start function inquirer

--- index.js
+++ index.js
@@ -18,3 +18,38 @@ connection.connect(function (err) {
     console.log("connected as id " + connection.threadId);
     start();
 });
+
+function start() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "start",
+            message: "Pick an action-",
+            chopices: [
+                "add a new department",
+                "add a new role",
+                "add a new employee",
+                "view all departments",
+                "view all roles",
+                "view all employees",
+                "update employee roles"
+            ]
+        }
+    ]).then(function (answer) {
+        if(answer.start === "add a new department") {
+            addDepartment();
+        } else if (answer.start === "add a new role") {
+            addRole();
+        } else if (answer.start === "add a new employee") {
+            addEmployee();
+        } else if (answer.start === "view all departments") {
+            viewDepartments();
+        } else if (answer.start === "view all roles") {
+            viewRoles();
+        } else if (answer.start === "view all employees") {
+            viewEmployees();
+        } else {
+            updateEmployeeRoles();
+        }
+    })
+}
